c5d34cd70567bc41e156904ce4c77baadfa3beae
Akaki <akakiburjanadze17@gmail.com>
2024-11-07 20:52:41 +0400

    StarSign.java

---
+++ PART2/StarSign.java
@@ -0,0 +1,16 @@
+package PART2;
+
+public class StarSign {
+    public static void main(String[] args) {
+        printStars(5);
+        printStars(3);
+        printStars(9);
+    }
+
+    public static void printStars(int number) {
+        for (int i = 0; i < number; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+}

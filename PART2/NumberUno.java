ea963aac1a7d0adf5a35bcacaba54cc09686d555
Akaki <akakiburjanadze17@gmail.com>
2024-11-09 14:56:45 +0400

    Add NumberUno.java

---
+++ PART2/NumberUno.java
@@ -0,0 +1,12 @@
+package PART2;
+
+public class NumberUno {
+    public static void main(String[] args) {
+        int result = numberUno();
+        System.out.println(result);
+    }
+
+    public static int numberUno() {
+        return 1;
+    }
+}

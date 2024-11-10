9ea3108f8500e3cc8a06d970e47ef58400edacf5
Akaki <akakiburjanadze17@gmail.com>
2024-11-10 14:11:11 +0400

    Add Word.java

---
+++ PART2/Word.java
@@ -0,0 +1,12 @@
+package PART2;
+
+public class Word {
+    public static void main(String[] args) {
+        String movie = word();
+        System.out.println(movie);
+    }
+
+    public static String word() {
+        return "Prison Break";
+    }
+}

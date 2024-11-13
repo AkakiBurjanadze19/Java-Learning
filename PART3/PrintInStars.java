5898213e3321de19e95d43bc2fbe9a80a4af3553
Akaki <akakiburjanadze17@gmail.com>
2024-11-13 18:11:44 +0400

    Add PrintInStars.java

---
+++ PART3/PrintInStars.java
@@ -0,0 +1,22 @@
+package PART3;
+
+public class PrintInStars {
+    public static void main(String[] args) {
+        int[] array = {5, 1, 3, 4, 2};
+        printArrayInStars(array);
+    }
+
+    public static void printStars(int size) {
+        for (int i = 0; i < size; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+
+    public static void printArrayInStars(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int size = array[i];
+            printStars(size);
+        }
+    }
+}

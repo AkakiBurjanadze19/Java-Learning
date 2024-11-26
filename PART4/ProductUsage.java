5579d601d5979af922bc64702f806a7311f228aa
Akaki <akakiburjanadze17@gmail.com>
2024-11-27 00:06:44 +0400

    Add ProductUsage.java

---
+++ PART4/ProductUsage.java
@@ -0,0 +1,8 @@
+package PART4;
+
+public class ProductUsage {
+    public static void main(String[] args) {
+        Product banana = new Product("banana", 1.1, 13);
+        banana.printProduct();
+    }
+}

715757e7bc8914b92330e885c9a0d2bee345741e
Akaki <akakiburjanadze17@gmail.com>
2024-11-27 01:10:04 +0400

    Add AgentUsage.java

---
+++ PART4/AgentUsage.java
@@ -0,0 +1,9 @@
+package PART4;
+
+public class AgentUsage {
+    public static void main(String[] args) {
+        Agent bond = new Agent("James", "Bond");
+
+        System.out.println(bond);
+    }
+}

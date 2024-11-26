4f70b0fcd8c48ef3ad6cd63629163db2ed6723f7
Akaki <akakiburjanadze17@gmail.com>
2024-11-27 00:28:10 +0400

    Add DebtUsage.java

---
+++ PART4/DebtUsage.java
@@ -0,0 +1,20 @@
+package PART4;
+
+public class DebtUsage {
+    public static void main(String[] args) {
+        Debt mortgage = new Debt(120000.0, 1.01);
+        mortgage.printBalance();
+
+        mortgage.waitOneYear();
+        mortgage.printBalance();
+
+        int years = 0;
+
+        while (years < 20) {
+            mortgage.waitOneYear();
+            years++;
+        }
+
+        mortgage.printBalance();
+    }
+}

c441f43ef3f5ff2e8235a0fc779c8b61660e3716
Akaki <akakiburjanadze17@gmail.com>
2024-11-27 00:57:54 +0400

    Add GaugeUsage.java

---
+++ PART4/GaugeUsage.java
@@ -0,0 +1,16 @@
+package PART4;
+
+public class GaugeUsage {
+    public static void main(String[] args) {
+        Gauge g = new Gauge();
+
+        while (!g.full()) {
+           System.out.println("Not full! Value: " + g.value());
+           g.increase();
+        }
+
+        System.out.println("Full! Value: " + g.value());
+        g.decrease();
+        System.out.println("Not full! Value: " + g.value());
+    }
+}

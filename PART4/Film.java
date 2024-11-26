876f2147bb34de8ee4f79c61460bf83bdd3ca0ba
Akaki <akakiburjanadze17@gmail.com>
2024-11-27 00:47:06 +0400

    Add Film.java

---
+++ PART4/Film.java
@@ -0,0 +1,19 @@
+package PART4;
+
+public class Film {
+    private String name;
+    private int ageRating;
+
+    public Film(String filmName, int filmAgeRating) {
+        this.name = filmName;
+        this.ageRating = filmAgeRating;
+    }
+
+    public String name() {
+        return this.name;
+    }
+
+    public int ageRating() {
+        return this.ageRating;
+    }
+}

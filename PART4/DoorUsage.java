9cb8dea161f5a2bc4af048b2b5e7be1cb1495d62
Akaki <akakiburjanadze17@gmail.com>
2024-11-26 23:58:45 +0400

    Add Door.java and DoorUsage.java

---
+++ PART4/DoorUsage.java
@@ -0,0 +1,10 @@
+package PART4;
+
+public class DoorUsage {
+    public static void main(String[] args) {
+        Door alexander = new Door();
+
+        Door.knock();
+        Door.knock();
+    }
+}

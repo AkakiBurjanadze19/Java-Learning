305264824d65a03f2beaa1e02b204e90091c81d0
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2024-12-06 20:58:24 +0400

    Add BookUsage.java

---
+++ PART5/BookUsage.java
@@ -0,0 +1,8 @@
+package PART5;
+
+public class BookUsage {
+    public static void main(String[] args) {
+        Book book = new Book("J.K Rowling", "Harry Potter and the Sorcerer's Stone", 223);
+        System.out.println(book);
+    }
+}

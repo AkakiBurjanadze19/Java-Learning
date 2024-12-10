aab3490d7e9dce65aa3429a4e589766010c1db0e
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2024-12-10 22:05:17 +0400

    Add HealthStationUsage.java

---
+++ PART5/HealthStationUsage.java
@@ -0,0 +1,22 @@
+package PART5;
+
+public class HealthStationUsage {
+    public static void main(String[] args) {
+        HealthStation childrensHospital = new HealthStation();
+
+        Person ethan = new Person("Ethan", 1, 110, 7);
+        Person peter = new Person("Peter", 33, 176, 85);
+
+        System.out.println(ethan.getName() + " weight: " + childrensHospital.weigh(ethan) + " kilos");
+        System.out.println(peter.getName() + " weight: " + childrensHospital.weigh(peter) + " kilos");
+
+        childrensHospital.feed(ethan);
+        childrensHospital.feed(ethan);
+        childrensHospital.feed(ethan);
+
+        System.out.println("");
+
+        System.out.println(ethan.getName() + " weight: " + childrensHospital.weigh(ethan) + " kilos");
+        System.out.println(peter.getName() + " weight: " + childrensHospital.weigh(peter) + " kilos");
+    }
+}

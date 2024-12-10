909c07686ef54df0a65bebca51911fe37d9f741c
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2024-12-10 20:23:50 +0400

    Add NullPointerException.java

---
+++ PART5/NullPointerException.java
@@ -0,0 +1,8 @@
+package PART5;
+
+public class NullPointerException {
+    public static void main(String[] args) {
+        String name = null;
+        System.out.println(name.toString());
+    }
+}

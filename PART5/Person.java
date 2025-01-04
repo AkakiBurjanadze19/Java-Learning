52502d027fcca395c37ebb7129f1311ff5448039
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2025-01-04 17:30:31 +0400

    add equals method which compares two person objects

--- PART5/Person.java
+++ PART5/Person.java
@@ -4,23 +4,29 @@ public class Person {
     private String name;
     private int age;
     private Pet pet;
+    SimpleDate date;
     private int weight;
     private int height;
 
-    public Person(String name, int age, int weight, int height, Pet pet) {
+    public Person(String name, int age, int weight, int height, Pet pet, SimpleDate date) {
         this.name = name;
         this.age = age;
         this.pet = pet;
+        this.date = date;
         this.weight = weight;
         this.height = height;
     }
 
+    public Person(String name, int weight, int height, SimpleDate date) {
+        this(name, 0, weight, height, null, date);
+    }
+
     public Person(String name, Pet pet) {
-        this(name, 0, 0, 0, pet);
+        this(name, 0, 0, 0, pet, null);
     }
 
     public Person(String name) {
-        this(name, 0, 0, 0, null);
+        this(name, 0, 0, 0, null, null);
     }
 
     public String getName() {
@@ -61,6 +67,36 @@ public class Person {
 
     public void setPet(String name, String breed) {
         this.pet = new Pet(name, breed);
+    }
+
+    public boolean equals(Object compared) {
+        if (this == compared) {
+            return true;
+        }
+
+        if (!(compared instanceof Person)) {
+            return false;
+        }
+
+        Person comparedPerson = (Person) compared;
+
+        // compare primitive fields
+        boolean nameEqual =
+                (this.name == null && comparedPerson.name == null || this.name != null && this.name.equals(comparedPerson.name));
+        boolean ageEqual = this.age == comparedPerson.age;
+        boolean weightEqual = this.weight == comparedPerson.weight;
+        boolean heightEqual = this.height == comparedPerson.height;
+
+        // compare pet objects
+        boolean petsEqual =
+                (this.pet == null && comparedPerson.pet == null) || (this.pet != null && comparedPerson.pet != null && this.pet.equals(comparedPerson.pet));
+
+        // compare date objects
+        boolean datesEqual =
+                (this.date == null && comparedPerson.date == null) || (this.date != null && comparedPerson.date != null && this.date.equals(comparedPerson.date));
+
+
+        return nameEqual && ageEqual && weightEqual && heightEqual && petsEqual && datesEqual;
     }
 
     public String toString() {

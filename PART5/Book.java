c16899991f988e56ba49bd21f2793c6de8cdd255
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2025-01-05 18:57:35 +0400

    implemented an equals method which compares two book objects

--- PART5/Book.java
+++ PART5/Book.java
@@ -22,6 +22,27 @@ public class Book {
     public int getPages() {
         return this.pages;
     }
+
+    public boolean equals(Object compared) {
+        if (this == compared) {
+            return true;
+        }
+
+        if (!(compared instanceof Book)) {
+            return false;
+        }
+
+        Book comparedBook = (Book) compared;
+
+        boolean authorEqual =
+                (this.author == null && comparedBook.author == null) || (this.author != null && comparedBook.author != null && this.author.equals(comparedBook.author));
+        boolean nameEqual =
+                (this.name == null && comparedBook.name == null) || (this.name != null && comparedBook.name != null && this.name.equals(comparedBook.name));
+        boolean pageEqual = this.pages == comparedBook.pages;
+
+        return authorEqual && nameEqual && pageEqual;
+    }
+
     public String toString() {
         return this.getAuthor() + ", " + this.getName() + ", " + this.getPages() + " pages";
     }

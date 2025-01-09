1cf754cc9080af12d144620e467761ad58d7eed4
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2025-01-09 15:20:29 +0400

    add MoneyUsage.java

---
+++ PART5/MoneyUsage.java
@@ -0,0 +1,20 @@
+package PART5;
+
+public class MoneyUsage {
+    public static void main(String[] args) {
+        Money a = new Money(10,0);
+        Money b = new Money(5,0);
+
+        Money c = a.plus(b);
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+        a = a.plus(c);
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}

8d57e924362d4139bf79188617c8e87bcb0b24a9
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2025-01-09 15:20:18 +0400

    add Money.java

---
+++ PART5/Money.java
@@ -0,0 +1,32 @@
+package PART5;
+
+public class Money {
+    private final int euros;
+    private final int cents;
+
+    public Money(int euros, int cents) {
+        this.euros = euros;
+        this.cents = cents;
+    }
+
+    public int euros() {
+        return this.euros;
+    }
+
+    public int cents() {
+        return this.cents;
+    }
+
+    public Money plus(Money addition) {
+        return new Money(this.euros + addition.euros, this.cents + addition.cents);
+    }
+
+    public String toString() {
+        String zero = "";
+        if (this.cents < 10) {
+            zero = "0";
+        }
+
+        return this.euros + "." + zero + cents + "e";
+    }
+}

68f7b6b8fc55f617fd966ec9c2dc7bdb0c5eaa18
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2025-01-10 16:08:46 +0400

    implement an addMeal method for adding new meals

--- PART6/Menu.java
+++ PART6/Menu.java
@@ -3,15 +3,23 @@ package PART6;
 import java.util.ArrayList;
 
 public class Menu {
-    private ArrayList<String> meals;
+    private ArrayList<String>
+            meals;
 
     public Menu() {
-        this.meals = new ArrayList<>();
+        this.meals =
+                new ArrayList<>();
     }
 
     public void addMeal(String meal) {
         if (!this.meals.contains(meal)) {
             this.meals.add(meal);
+        }
+    }
+
+    public void printMeals() {
+        for (String meal : meals) {
+            System.out.println(meal);
         }
     }
 }

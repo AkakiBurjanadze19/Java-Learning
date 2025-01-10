423b93a1e728d7968b6e85c17fb770935aacaab1
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2025-01-10 16:09:04 +0400

    test adding new meals and printing them

--- PART6/MenuUsage.java
+++ PART6/MenuUsage.java
@@ -2,6 +2,13 @@ package PART6;
 
 public class MenuUsage {
     public static void main(String[] args) {
-        
+        Menu menu = new Menu();
+
+        menu.addMeal("Tofu ratatouille");
+        menu.addMeal("Chilli Coconut Chicken");
+        menu.addMeal("Chilli Coconut Chicken");
+        menu.addMeal("Meatballs with mustard sauce");
+
+        menu.printMeals();
     }
 }

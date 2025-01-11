d79a42bcaadf6ab8774add2fa236050a17820868
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2025-01-11 15:30:41 +0400

    test take method

--- PART6/StackUsage.java
+++ PART6/StackUsage.java
@@ -2,11 +2,26 @@ package PART6;
 
 public class StackUsage {
     public static void main(String[] args) {
+//        Stack s = new Stack();
+//        System.out.println(s.isEmpty());
+//        System.out.println(s.values());
+//        s.add("Value");
+//        System.out.println(s.isEmpty());
+//        System.out.println(s.values());
+//        String taken = s.take();
+//        System.out.println(s.isEmpty());
+//        System.out.println(s.values());
+//        System.out.println(taken);
+
         Stack s = new Stack();
-        System.out.println(s.isEmpty());
-        System.out.println(s.values());
-        s.add("Value");
-        System.out.println(s.isEmpty());
-        System.out.println(s.values());
+        s.add("1");
+        s.add("2");
+        s.add("3");
+        s.add("4");
+        s.add("5");
+
+        while (!s.isEmpty()) {
+            System.out.println(s.take());
+        }
     }
 }

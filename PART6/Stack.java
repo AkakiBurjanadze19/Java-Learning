8257940b0c932dd798e08e1bf983502afbc02120
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2025-01-11 15:20:22 +0400

    add Stack.java

---
+++ PART6/Stack.java
@@ -0,0 +1,11 @@
+package PART6;
+
+import java.util.ArrayList;
+
+public class Stack {
+    private ArrayList<String> items;
+
+    public boolean isEmpty() {
+        return items.isEmpty();
+    }
+}

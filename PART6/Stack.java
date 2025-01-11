bfba5099ae9b6ed64719885530b07ec644e6b7b5
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2025-01-11 15:21:26 +0400

    modify isEmpty method and implement add method

--- PART6/Stack.java
+++ PART6/Stack.java
@@ -6,6 +6,10 @@ public class Stack {
     private ArrayList<String> items;
 
     public boolean isEmpty() {
-        return items.isEmpty();
+        return this.items.isEmpty();
+    }
+
+    public void add(String value) {
+        this.items.add(value);
     }
 }

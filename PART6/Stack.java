e1258b9241fcc67cef4d58f0217e0b94ee741c4d
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2025-01-11 15:25:26 +0400

    fix Stack class by adding a constructor and implement values method

--- PART6/Stack.java
+++ PART6/Stack.java
@@ -5,11 +5,19 @@ import java.util.ArrayList;
 public class Stack {
     private ArrayList<String> items;
 
+    public Stack() {
+        this.items = new ArrayList<>();
+    }
+
     public boolean isEmpty() {
         return this.items.isEmpty();
     }
 
     public void add(String value) {
         this.items.add(value);
+    }
+
+    public ArrayList<String> values() {
+        return this.items;
     }
 }

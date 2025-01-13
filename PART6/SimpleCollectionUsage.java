d9f156093f5c5d2ad06ed93fda28a89619c04115
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2025-01-13 15:58:01 +0400

    add SimpleCollectionUsage.java

---
+++ PART6/SimpleCollectionUsage.java
@@ -0,0 +1,16 @@
+package PART6;
+
+public class SimpleCollectionUsage {
+    public static void main(String[] args) {
+        SimpleCollection s = new SimpleCollection("alphabet");
+
+        s.add("a");
+        System.out.println(s);
+
+        s.add("b");
+        System.out.println(s);
+
+        s.add("c");
+        System.out.println(s);
+    }
+}

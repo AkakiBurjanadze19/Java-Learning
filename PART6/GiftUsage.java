22b1634ef1a5b48392547e784f998bb8d2bb8619
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2025-01-14 22:26:05 +0400

    add GiftUsage.java

---
+++ PART6/GiftUsage.java
@@ -0,0 +1,12 @@
+package PART6;
+
+public class GiftUsage {
+    public static void main(String[] args) {
+        Gift book = new Gift("Harry Potter and the Philosopher's Stone", 2);
+
+        System.out.println("Gift's name: " + book.getName());
+        System.out.println("Gift's weight: " + book.getWeight());
+
+        System.out.println("Gift: " + book);
+    }
+}

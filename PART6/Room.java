86460c548498ca5884a894f7dd9999fb2900dc12
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2025-01-17 20:31:51 +0400

    add Room.java

---
+++ PART6/Room.java
@@ -0,0 +1,23 @@
+package PART6;
+
+import java.util.ArrayList;
+
+public class Room {
+    private ArrayList<Person> persons;
+
+    public Room() {
+        this.persons = new ArrayList<>();
+    }
+
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+
+    public boolean isEmpty() {
+        return this.persons.isEmpty();
+    }
+
+    public ArrayList<Person> getPersons() {
+        return this.persons;
+    }
+}
